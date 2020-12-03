030bb3967de99eed9d345e0fa371f3c458d6d70f
sy19304037 <sy19304037@ga.tera-house.ac.jp>
2020-12-03 15:47:00 +0900

    item表、user表の訂正

--- bat/MySQLCreate.sql
+++ bat/MySQLCreate.sql
@@ -1,31 +1,31 @@
 create table user(
-user_id int UNSIGNED auto_increment,
+user_id int UNSIGNED,
 user_name varchar(20),
 real_name varchar(10),
 address varchar(20),
-tel varchar(10),
+tel varchar(20),
 mail varchar(25),
 profile varchar(500),
 point int UNSIGNED,
  primary key(user_id),
- UNIQUE(address)
+ UNIQUE(mail)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE table hardware(
-hardware_id int UNSIGNED auto_increment,
+hardware_id int UNSIGNED,
 hardware varchar(20),
 primary key(hardware_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 create table category(
-category_id int UNSIGNED auto_increment,
+category_id int UNSIGNED,
 category varchar(20),
 primary key(category_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 create table item(
-item_id int unsigned PRIMARY KEY auto_increment,
+item_id int unsigned PRIMARY KEY,
 item_name varchar(40),
 price int UNSIGNED,
 item_image varchar(40),
@@ -35,13 +35,19 @@ category_id int UNSIGNED,
 seller_id int UNSIGNED,
 buyer_address varchar(30),
 term datetime,
+stock int DEFAULT 1,
+exhibit_date datetime,
 CONSTRAINT fk_hardware_id
-    FOREIGN KEY hardware_id(hardware_id)
+    FOREIGN KEY (hardware_id)
     REFERENCES hardware(hardware_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT,
 CONSTRAINT fk_category_id
-    FOREIGN KEY category_id(category_id)
+    FOREIGN KEY (category_id)
     REFERENCES category(category_id)
+    ON DELETE RESTRICT ON UPDATE RESTRICT,
+CONSTRAINT fk_item_seller_id
+    FOREIGN KEY (seller_id)
+    REFERENCES user(user_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT,
 CONSTRAINT fk_buyer_address
     FOREIGN KEY (buyer_address)
@@ -51,57 +57,57 @@ CONSTRAINT fk_buyer_address
 
 
 create table PaymentLog(
-payment_id int unsigned PRIMARY KEY auto_increment,
+payment_id int unsigned PRIMARY KEY,
 seller_id int UNSIGNED,
 buyer_id int UNSIGNED,
 item_id int UNSIGNED,
 price int UNSIGNED,
 date DATETIME,
 CONSTRAINT fk_seller_id
-    FOREIGN KEY seller_id(seller_id)
+    FOREIGN KEY seller_id(seller_id) 
     REFERENCES user(user_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT,
 CONSTRAINT fk_buyer_id
-    FOREIGN KEY buyer_id(buyer_id)
+    FOREIGN KEY buyer_id(buyer_id) 
     REFERENCES user(user_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 create table Deal(
-deal_id int unsigned PRIMARY KEY auto_increment,
+deal_id int unsigned PRIMARY KEY,
 payment_id int UNSIGNED,
 deposit_id int UNSIGNED,
 item_id int UNSIGNED,
 deal_state int UNSIGNED,
 time_limit DATETIME,
 CONSTRAINT fk_payment_id
-    FOREIGN KEY payment_id(payment_id)
+    FOREIGN KEY payment_id(payment_id) 
     REFERENCES PaymentLog(payment_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT,
 CONSTRAINT fk_deposit_id
-    FOREIGN KEY payment_id(deposit_id)
+    FOREIGN KEY payment_id(deposit_id) 
     REFERENCES PaymentLog(payment_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT,
 CONSTRAINT fk_item_id
-    FOREIGN KEY (item_id)
+    FOREIGN KEY (item_id) 
     REFERENCES item(item_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 create table Notice(
-notice_id int UNSIGNED auto_increment,
+notice_id int UNSIGNED,
 user_id int UNSIGNED,
 comment varchar(500),
  primary key(notice_id),
 CONSTRAINT fk_user_id
-    FOREIGN KEY (user_id)
+    FOREIGN KEY (user_id) 
     REFERENCES user(user_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 create table chat(
-chat_id int UNSIGNED auto_increment,
+chat_id int UNSIGNED,
 payment_id int UNSIGNED,
 buyer_id int UNSIGNED,
 seller_id int UNSIGNED,
@@ -109,24 +115,20 @@ text varchar(500),
 date datetime,
 primary key(chat_id),
 CONSTRAINT fk_chat_payment_id
-    FOREIGN KEY (payment_id)
+    FOREIGN KEY (payment_id) 
     REFERENCES paymentlog(payment_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT,
 CONSTRAINT fk_chat_buyer_id
-    FOREIGN KEY (buyer_id)
+    FOREIGN KEY (buyer_id) 
     REFERENCES user(user_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT,
 CONSTRAINT fk_chat_seller_id
-    FOREIGN KEY (seller_id)
+    FOREIGN KEY (seller_id) 
     REFERENCES user(user_id)
     ON DELETE RESTRICT ON UPDATE RESTRICT
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 commit;
-
-
-
-
 
 
 
