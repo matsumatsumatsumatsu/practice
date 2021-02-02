270726aeb4439196f72d0c4f5809a0508fdca3c1
ti19304004 <ti19304004@ga.tera-house.ac.jp>
2021-02-02 10:46:50 +0900

    ShowItemListCommandの追加(未完成)

---
+++ mercari/WEB-INF/classes/command/ShowItemListCommand.java
@@ -0,0 +1,21 @@
+package command;
+
+import java.util.List;
+
+import context.ResponseContext;
+import dao.AbstractMysqlFactory;
+import dao.ItemInterfaceDao;
+
+public class ShowItemListCommand  extends AbstractCommand {
+	public  ResponseContext execute(ResponseContext resc){
+
+    	AbstractMysqlFactory factory=AbstractMysqlFactory.getFactory();
+        ItemInterfaceDao dao=factory.getItemInterfaceDao();
+
+        List items = dao.getAllItems();
+
+        resc.setResult(items);
+        resc.setTarget("view");
+        return resc;
+    }
+}
