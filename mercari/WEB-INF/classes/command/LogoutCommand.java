255417e15415a670675fd955fdbe963e6d063c69
ys19304019 <ys19304019@ga.tera-house.ac.jp>
2021-02-02 10:04:11 +0900

    logout

--- mercari/WEB-INF/classes/command/LogoutCommand.java
+++ mercari/WEB-INF/classes/command/LogoutCommand.java
@@ -6,8 +6,8 @@ import util.SessionManager;
 
 public class LogoutCommand extends AbstractCommand{
 	public ResponseContext execute(ResponseContext resc){
-		RequestContext reqc=getRequestContext();
-		SessionManager session=new SessionManager(reqc);
+		RequestContext reqc = getRequestContext();
+		SessionManager session = new SessionManager(reqc);
 		session.invalidate();
 		resc.setTarget("start");
 		return resc;
