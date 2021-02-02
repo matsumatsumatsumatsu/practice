a3c9be792b60df778c332061473ce7ba5f91de60
Nishimi <rn19304027@ga.tera-house.ac.jp>
2021-02-02 10:03:18 +0900

    logincommand

--- mercari/WEB-INF/classes/command/LoginCommand.java
+++ mercari/WEB-INF/classes/command/LoginCommand.java
@@ -2,14 +2,23 @@ package command;
 
 import context.RequestContext;
 import context.ResponseContext;
-
+import dao.AbstractMysqlFactory;
+import dao.UserInterfaceDao;
 
 public class LoginCommand extends AbstractCommand{
 	public ResponseContext execute(ResponseContext resc) {
-		RequestContext rq = getRequestContext();
+		RequestContext rq=  getRequestContext();
 
+		String userNames[] =rq.getParameter("userName");
+        String userName=userNames[0];
+        String userPasswords[] =rq.getParameter("userPassword");
+        String userPassword=userPasswords[0];
 
-		return resc;
+        AbstractMysqlFactory factory=AbstractMysqlFactory.getFactory();
+        UserInterfaceDao dao=factory.getUserInterfaceDao();
+
+        dao.getUser(userName);
+		resc.setTarget("start");
+        return resc;
 	}
-
 }
