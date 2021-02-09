9537ffa05448770e07b0b755dc5a7d9022e93fe1
Nishimi <rn19304027@ga.tera-house.ac.jp>
2021-02-09 16:25:53 +0900

    paymentLog,privateChat Bean;

---
+++ mercari/WEB-INF/classes/bean/PaymentLog.java
@@ -0,0 +1,68 @@
+package bean;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class PaymentLog implements Serializable{
+
+	private String paymentId;
+	private String sellerId;
+	private String buyerId;
+	private String itemid;
+	private int price;
+	private Timestamp date;
+
+	public PaymentLog() {}
+
+	public String getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(String paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public String getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(String sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public String getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(String buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	public String getItemid() {
+		return itemid;
+	}
+
+	public void setItemid(String itemid) {
+		this.itemid = itemid;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public Timestamp getDate() {
+		return date;
+	}
+
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+
+
+
+
+}
