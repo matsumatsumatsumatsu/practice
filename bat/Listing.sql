ebe11fa3de422b268882b0a0fb2bd41532d0099e
ti19304004 <54879332+ti19304004@users.noreply.github.com>
2021-02-17 14:36:42 +0900

    Add files via upload
    出品用batファイル追加

---
+++ bat/Listing.sql
@@ -0,0 +1,7 @@
+insert into item(item_name, price, item_image, item_explanation, hardware_id, category_id, seller_id, listing_date, term) values('samlple1',100,'','サンプル用ダミー',1,1,2,cast( now() as datetime),1);
+insert into item(item_name, price, item_image, item_explanation, hardware_id, category_id, seller_id, listing_date, term) values('samlple2',100,'','サンプル用ダミー',1,1,2,cast( now() as datetime),1);
+insert into item(item_name, price, item_image, item_explanation, hardware_id, category_id, seller_id, listing_date, term) values('samlple3',100,'','サンプル用ダミー',1,1,2,cast( now() as datetime),1);
+insert into item(item_name, price, item_image, item_explanation, hardware_id, category_id, seller_id, listing_date, term) values('samlple4',100,'','サンプル用ダミー',1,1,2,cast( now() as datetime),1);
+insert into item(item_name, price, item_image, item_explanation, hardware_id, category_id, seller_id, listing_date, term) values('samlple5',100,'','サンプル用ダミー',1,1,2,cast( now() as datetime),1);
+
+commit;
