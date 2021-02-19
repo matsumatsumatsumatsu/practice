fe19313c2eeb69f320f195d55f25d01cf3b30821
sy19304037 <sy19304037@ga.tera-house.ac.jp>
2021-02-19 15:47:10 +0900

    purchase.jspの実装＋コメントの追加

--- mercari/WEB-INF/classes/command/ShowPurchaseCommand.java
+++ mercari/WEB-INF/classes/command/ShowPurchaseCommand.java
@@ -12,64 +12,43 @@ import dao.UserInterfaceDao;
 import exception.IntegrationException;
 import util.SessionManager;
 
-public class ShowProfileCommand extends AbstractCommand{
+public class ShowPurchaseCommand extends AbstractCommand {
 	public ResponseContext execute(ResponseContext resc) {
 		RequestContext reqc = getRequestContext();
 
 		//ログインしているユーザーのIDを取得する
 		SessionManager.getSession(reqc);
-//		System.out.println("token:"+SessionManager.getAttribute("token"));
+
 		String sessionUserId=((User)SessionManager.getAttribute("token")).getUserId();
-//	    System.out.println("user_id="+sessionUserId);
+
 
 		AbstractMysqlFactory factory=AbstractMysqlFactory.getFactory();
 		UserInterfaceDao userdao = factory.getUserInterfaceDao();
 		DealInterfaceDao dealdao = factory.getDealInterfaceDao();
 
-
-		List user = new ArrayList(); //ユーザーの情報
+		//ユーザーの情報
+//		List user = new ArrayList();
 		//購入した取引中の取引一覧
 		List buyDeal = new ArrayList();
 
         try {
-        	user = userdao.getUser(sessionUserId);
+//        	user = userdao.getUser(sessionUserId);
         	buyDeal = dealdao.getAllDeals(sessionUserId);
 
         }catch(IntegrationException e) {
         	//例外処理
 
         }
+		List<Object> first=new ArrayList<>();
+		first.add("buyDeal");
+		first.add(buyDeal);
 
-		List sellDeal = new ArrayList();
-
-        try {
-        	sellDeal = dealdao.getSellAllDeals(sessionUserId);
-
-        }catch(IntegrationException e) {
-        }
-
-		//出品した取引中の取引一覧
-
-
-        List<Object> first=new ArrayList<>();
-		first.add("data");
-		first.add(user);
-
-		List<Object> second=new ArrayList<>();
-		second.add("buyDeal");
-		second.add(buyDeal);
-
-		List<Object> third=new ArrayList<>();
-		third.add("sellDeal");
-		third.add(sellDeal);
 
 		List<List> result=new ArrayList<>();
 		result.add(first);
-		result.add(second);
-		result.add(third);
 
 		resc.setResult(result);
-		resc.setTarget("mypage");
+		resc.setTarget("purchase");
 		return resc;
 	}
 }
