7d096b9537b3c780624618471f56e54fae2fb86b
ys19304019 <ys19304019@ga.tera-house.ac.jp>
2021-02-20 16:58:34 +0900

    通知今度こそ完了

--- mercari/WEB-INF/classes/dao/ItemInterfaceDao.java
+++ mercari/WEB-INF/classes/dao/ItemInterfaceDao.java
@@ -9,7 +9,7 @@ public interface ItemInterfaceDao {
 	public void listing(Item item) throws IntegrationException;
 	public List getAllItems() throws IntegrationException;
 	public List getItem(String key) throws IntegrationException;
-	public String getItemName(String userId) throws IntegrationException;
+	public String getItemName(String itemId) throws IntegrationException;
 	public String  getSellerId(String itemId) throws IntegrationException;
 	public Item manageStock(String itemId) throws IntegrationException;
 	public List search(String keyword) throws IntegrationException;
