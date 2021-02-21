d0142f024050736a49b55fd35551058909cbb141
Sak <54879453+S4K08@users.noreply.github.com>
2021-02-21 17:41:09 +0900

    管理者IDは1000から連番に仕様変更

--- bat/MySQLCreate.sql
+++ bat/MySQLCreate.sql
@@ -173,16 +173,16 @@ insert into category(category_id, category)
 values(1, 'RPG');
 
 insert into category(category_id, category)
-values(2, 'アクション');
+values(2, '繧｢繧ｯ繧ｷ繝ｧ繝ｳ');
 
 insert into category(category_id, category)
-values(3, 'パズル');
+values(3, '繝代ぜ繝ｫ');
 
 insert into category(category_id, category)
-values(4, 'アドベンチャー');
+values(4, '繧｢繝峨�吶Φ繝√Ε繝ｼ');
 
-insert into admin(admin_name,admin_password,mail) 
-values('admin','P@ssw0rd','info@gmail.com');
+insert into admin(admin_id,admin_name,admin_password,mail)
+values(1000,'admin','P@ssw0rd','info@gmail.com');
 
 
 commit;
