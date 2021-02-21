6496c0acadc42b82a9c9f98bb5838248627b122d
ys19304019 <ys19304019@ga.tera-house.ac.jp>
2021-02-21 16:11:08 +0900

    商品の受け取りを相手に通知する処理の実装

--- mercari/WEB-INF/classes/command/ReceiveItemCommand.java
+++ mercari/WEB-INF/classes/command/ReceiveItemCommand.java
@@ -1,25 +1,30 @@
 package command;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Deal;
 import context.RequestContext;
 import context.ResponseContext;
 import dao.AbstractMysqlFactory;
 import dao.DealInterfaceDao;
+import dao.ItemInterfaceDao;
+import exception.BusinessLogicException;
 import exception.IntegrationException;
 
 public class ReceiveItemCommand extends AbstractCommand{
-	public ResponseContext execute(ResponseContext resc) {
+	public ResponseContext execute(ResponseContext resc)  throws BusinessLogicException{
 
 		RequestContext reqc = getRequestContext();
 
-
-
 		AbstractMysqlFactory factory=AbstractMysqlFactory.getFactory();
-		DealInterfaceDao dao = factory.getDealInterfaceDao();
+		DealInterfaceDao dealDao = factory.getDealInterfaceDao();
+		ItemInterfaceDao itemDao = factory.getItemInterfaceDao();
 
 		String  dealId= reqc.getParameter("deal_id")[0];
 
 		try{
-			dao.changeState(dealId, "3");
+			dealDao.changeState(dealId, "3");
 		}catch(IntegrationException e) {
 			//例外処理
 		}
@@ -27,6 +32,18 @@ public class ReceiveItemCommand extends AbstractCommand{
 		ShowDealingInfoCommand show = new ShowDealingInfoCommand();
 		show.init(reqc);
 		resc = show.execute(resc);
+
+		//通知
+		//売り手の発送した商品が受け取られた旨を通知する
+		List deal = new ArrayList();
+		deal = dealDao.getDeal(dealId);
+		String itemId = ((Deal)deal.get(0)).getItemId();
+		String sellerId = itemDao.getSellerId(itemId);
+		String sellerComment = itemDao.getItemName(itemId) + "を購入者様が受け取りました";
+
+		NotifyCommand notify  = new NotifyCommand(sellerId,sellerComment);
+		notify.init(reqc);
+		resc = notify.execute(resc);
 
 		return resc;
 	}
