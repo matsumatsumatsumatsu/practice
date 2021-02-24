c26e3c6820f7c27536b9e9d9424fd2635d0a2dcf
sy19304037 <sy19304037@ga.tera-house.ac.jp>
2021-02-24 12:32:30 +0900

    partの仮

--- mercari/WEB-INF/classes/context/RequestContext.java
+++ mercari/WEB-INF/classes/context/RequestContext.java
@@ -1,8 +1,11 @@
 package context;
 
+import java.util.Collection;
+
 public interface RequestContext{
     public String getCommandPath();
     public String[] getParameter(String key);
     public Object getRequest();
     public void setRequest(Object request);
+    public Collection getParts();
 }
