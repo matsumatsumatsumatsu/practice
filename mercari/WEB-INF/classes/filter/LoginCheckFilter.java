454846a61b6f10ba2956e615ad5e2ae2f94978d1
sy19304037 <sy19304037@ga.tera-house.ac.jp>
2021-02-26 12:10:45 +0900

    再修正！！！

--- mercari/WEB-INF/classes/filter/LoginCheckFilter.java
+++ mercari/WEB-INF/classes/filter/LoginCheckFilter.java
@@ -8,22 +8,20 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 public class LoginCheckFilter implements Filter{
   public void init(FilterConfig config)throws ServletException{}
   public void destroy(){}
 
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)throws IOException, ServletException{
-	HttpServletRequest hreq = (HttpServletRequest)req;
-	HttpSession session=((HttpServletRequest) req).getSession();
-
-    Object flag=session.getAttribute("token");
-
-    if(flag != null) {
-    	hreq.setAttribute("flag", "OK");
-    }
+//	HttpServletRequest hreq = (HttpServletRequest)req;
+//	HttpSession session=((HttpServletRequest) req).getSession();
+//
+//    Object flag=session.getAttribute("token");
+//
+//    if(flag != null) {
+//    	hreq.setAttribute("flag", "OK");
+//    }
 
     chain.doFilter(req,res);
 
