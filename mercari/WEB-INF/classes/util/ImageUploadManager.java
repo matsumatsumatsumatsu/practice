1fecaacb60d576eb051e06ff67ccbbe5923da826
Sak <54879453+S4K08@users.noreply.github.com>
2021-05-07 09:41:41 +0900

    Update ImageUploadManager.java

--- mercari/WEB-INF/classes/util/ImageUploadManager.java
+++ mercari/WEB-INF/classes/util/ImageUploadManager.java
@@ -47,7 +47,7 @@ public class ImageUploadManager {
 					if ((filename != null) && (!filename.equals(""))){
 						filename = UUID.randomUUID().toString()+".jpg";
 						item.write(new File("C:\\Users\\koyama\\git\\practice\\mercari\\WebContent\\images\\" + filename));
-						//item.write(new File("\\172.19.253.32\\public\\2020\\卒業制作\\mercari\\images\\" + filename)); //共有にあげるほう
+						//item.write(new File("\\***.**.***.**\\public\\2020\\卒業制作\\mercari\\images\\" + filename)); //共有にあげるほう
 						//item.write(new File("/webapps/2020/mercari/images/" + filename));
 						fields.put("itemImage",filename);
 					}
